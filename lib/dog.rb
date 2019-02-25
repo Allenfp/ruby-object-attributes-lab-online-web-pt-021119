6c95a97af8a7d1434c98046daf9628ebcfcb4183
Allen F Partlow <allenfp@gmail.com>
2019-02-25 06:48:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,4 @@
 class Dog
-  
-  def name=(dog_name)
-    @name = dog_name
-  end
-
-  def name
-    @name
-  end
 
   def name=(dog_name)
     @name = dog_name
@@ -14,6 +6,14 @@ class Dog
 
   def name
     @name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def name
+    @breed
   end
 
 end
